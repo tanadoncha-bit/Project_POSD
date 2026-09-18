8564752d06bde5e944d2b3f6ace4c904ff978871
kanyaphak-th06 <kanyaphak.th@kkumail.com>
2026-09-19 04:16:23 +0700

    Add dashboard controller and page styling

--- src/main/java/com/example/itborrow/controller/web/DashboardController.java
+++ src/main/java/com/example/itborrow/controller/web/DashboardController.java
@@ -1,5 +1,15 @@
 package com.example.itborrow.controller.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class DashboardController {
+
+    @GetMapping({"/", "Dashboard"})
+    public String showDashboard() {
+        return "dashboard/index";
+    }
+    
     
 }
