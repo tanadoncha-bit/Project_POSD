51479cf67fcf2e95dd12cc7981f0d631ad29d427
alichac-afk <alicha.c@kkumail.com>
2026-09-21 13:45:08 +0700

    add UserController,EquipmentController and edit service

--- src/main/java/com/example/itborrow/repository/EquipmentRepository.java
+++ src/main/java/com/example/itborrow/repository/EquipmentRepository.java
@@ -1,9 +1,11 @@
 package com.example.itborrow.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.itborrow.domain.entity.Equipment;
 
-public interface EquipmentRepository extends JpaRepository<Equipment, Long> {
+@Repository 
+public interface EquipmentRepository extends JpaRepository<Equipment,Long> {
     
 }
